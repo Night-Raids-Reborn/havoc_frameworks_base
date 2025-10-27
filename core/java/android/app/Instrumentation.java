e5f101e719a9f249322101150800471bcd37d505
darknight <darkniuslightness@gmail.com>
2025-10-27 09:35:04 +0000

    Revert "PIHooks: Get package name from context instead of app"
    This reverts commit ceff8674947c676a041f3896ff00b94b7285eefe.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1158,7 +1158,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PropImitationHooks.setProps(context);
+        PropImitationHooks.setProps(app);
         return app;
     }
     
@@ -1176,7 +1176,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PropImitationHooks.setProps(context);
+        PropImitationHooks.setProps(app);
         return app;
     }
 
